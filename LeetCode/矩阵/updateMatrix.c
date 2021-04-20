330a367c169cd73dec28c555d203eb1c6d29cfa2
刘彬彬 <956762794@qq.com>
2021-04-20 10:55:01 +0800

    矩阵 队列

--- LeetCode/矩阵/updateMatrix.c
+++ LeetCode/矩阵/updateMatrix.c
@@ -20,56 +20,44 @@ int **updateMatrix(int **matrix, int matrixSize, int *matrixColSize, int *return
   for (int t = 0; t < m; t++)
   {
     matrixArr[t] = (int *)malloc(sizeof(int) * n);
-    memset(matrixArr[t], 0, sizeof(int) * n);
+    memset(matrixArr[t], 1000001, sizeof(int) * n);
   }
 
   int arr_x[4] = {-1, 1, 0, 0};
   int arr_y[4] = {0, 0, 1, -1};
   //Bsf  malloc queue sizem*n
   Node *queue = (Node *)malloc(sizeof(Node) * m * n);
+  int end = 0;
+  int start = 0;
+  //It find a number equal to 0 add the queue
   for (int i = 0; i < m; i++)
   {
     for (int j = 0; j < n; j++)
     {
-      int start = 0;
-      int end = 1;
-      if (matrix[i][j] != 0)
+      if (matrix[i][j] == 0)
       {
-        queue[start].x = i;
-        queue[start].y = j;
-        int count = 0;
-        //in queue
-        while (start != end)
-        {
-          int new_end = end;
-          while (start != new_end)
-          {
-            for (int f = 0; f < 4; f++)
-            {
-              int new_x = queue[start].x + arr_x[f];
-              int new_y = queue[start].y + arr_y[f];
-              int tt = queue[start].y;
-              if (new_x >= 0 && new_x < m && new_y >= 0 && new_y < n)
-              {
-                if (matrix[new_x][new_y] != 0)
-                {
-                  queue[end++].x = new_x;
-                  queue[end].y = new_y;
-                }
-                else
-                {
-                  end = new_end;
-                  break;
-                }
-              }
-            }
-            start++;
-          }
-          count++;
-        }
-        matrixArr[i][j] = count;
+        queue[end].x = i;
+        queue[end++].y = j;
+        matrixArr[i][j] = 0;
       }
     }
+  }
+
+  while (start != end)
+  {
+    for (int f = 0; f < 4; f++)
+    {
+      int new_x = queue[start].x + arr_x[f];
+      int new_y = queue[start].y + arr_y[f];
+      if (
+          new_x >= 0 && new_x < m && new_y >= 0 && new_y < n && (matrixArr[queue[start].x][queue[start].y] + 1 < matrixArr[new_x][new_y]))
+      {
+        matrixArr[new_x][new_y] = matrixArr[queue[start].x][queue[start].y] + 1;
+        queue[end].x = new_x;
+        queue[end++].y = new_y;
+      }
+    }
+    start++;
   }
   return matrixArr;
 }
