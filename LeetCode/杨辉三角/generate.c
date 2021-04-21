abaa7ddb613cea30c030bcdff38beb0bb9907958
刘彬彬 <956762794@qq.com>
2021-04-21 11:54:30 +0800

    杨辉三角递归

--- LeetCode/杨辉三角/generate.c
+++ LeetCode/杨辉三角/generate.c
@@ -20,3 +20,25 @@ int **generate(int numRows, int *returnSize, int **returnColumnSizes)
   }
   return ret;
 }
+//------------------------递归----------------------------------------------------
+int **generate2(int numRows, int *returnSize, int **returnColumnSizes)
+{
+  *returnSize = numRows;
+  *returnColumnSizes = (int *)malloc(sizeof(int) * numRows);
+  int **ret = (int **)malloc(sizeof(int *) * numRows);
+  return fillGenerate(ret, 0, numRows, returnColumnSizes);
+}
+
+int **fillGenerate(int **ret, int rows, int numRows, int **returnColumnSizes)
+{
+  if (rows > numRows - 1)
+    return ret;
+  ret[rows] = (int *)malloc(sizeof(int) * (rows + 1));
+  (*returnColumnSizes)[rows] = rows + 1;
+  ret[rows][0] = ret[rows][rows] = 1;
+  for (int j = 1; j < rows; j++)
+  {
+    ret[rows][j] = ret[rows - 1][j] + ret[rows - 1][j - 1];
+  }
+  return fillGenerate(ret, rows + 1, numRows, returnColumnSizes);
+}
