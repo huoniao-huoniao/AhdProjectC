853a6705daf7c0a8920ed0d5699926bc4d71f8d1
刘彬彬 <956762794@qq.com>
2021-04-28 15:42:31 +0800

    构造二叉树

--- LeetCode/二叉树/从中序与后序遍历序列构造二叉树/buildTree.c
+++ LeetCode/二叉树/从中序与后序遍历序列构造二叉树/buildTree.c
@@ -6,8 +6,49 @@
  *     struct TreeNode *right;
  * };
  */
-
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+struct TreeNode *buildTree(int *inorder, int inorderSize, int *postorder, int postorderSize);
+struct TreeNode *buildNode(int *inorder, int inorserStart, int inorderEnd, int *postorder, int postorderStart, int postorderEnd);
+int findArraySize(int val, int *array, int start, int end);
+struct TreeNode
+{
+  int val;
+  struct TreeNode *left;
+  struct TreeNode *right;
+};
 struct TreeNode *buildTree(int *inorder, int inorderSize, int *postorder, int postorderSize)
 {
-  
+  return buildNode(inorder, 0, inorderSize - 1, postorder, 0, postorderSize - 1);
+}
+
+struct TreeNode *buildNode(int *inorder, int inorserStart, int inorderEnd, int *postorder, int postorderStart, int postorderEnd)
+{
+  if (inorderEnd < 0 || postorderEnd < 0 || inorserStart > inorderEnd)
+    return NULL;
+  if (inorserStart == inorderEnd)
+  {
+    struct TreeNode *root = (struct TreeNode *)malloc(sizeof(struct TreeNode));
+    root->val = inorder[inorserStart];
+    root->left = NULL;
+    root->right = NULL;
+    return root;
+  }
+  struct TreeNode *root = (struct TreeNode *)malloc(sizeof(struct TreeNode));
+  root->val = postorder[postorderEnd];
+  int i = findArraySize(root->val, inorder, inorserStart, inorderEnd);
+  root->left = buildNode(inorder, inorserStart, i - 1, postorder, postorderStart, (i - 1) - inorserStart + postorderStart);
+  root->right = buildNode(inorder, i + 1, inorderEnd, postorder, (postorderEnd - 1) - (inorderEnd - i - 1), postorderEnd - 1);
+  return root;
+}
+
+int findArraySize(int val, int *array, int start, int end)
+{
+  for (int i = start; i <= end; i++)
+  {
+    if (array[i] == val)
+      return i;
+  }
+  return -1;
 }
