ce0389ea21973567056ccf9ff3cee918ae7f16e6
刘彬彬 <956762794@qq.com>
2021-04-28 18:18:08 +0800

    二叉树的最近公共祖先

---
+++ LeetCode/二叉树/二叉树的最近公共祖先/lowestCommonAncestor.c
@@ -0,0 +1,12 @@
+#include <string.h>
+#include <stdlib.h>
+struct TreeNode
+{
+  int val;
+  struct TreeNode *left;
+  struct TreeNode *right;
+};
+struct TreeNode *lowestCommonAncestor(struct TreeNode *root, struct TreeNode *p, struct TreeNode *q)
+{
+  
+}
