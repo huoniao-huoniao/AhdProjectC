ca75b2029eeff81a8f3f5fcc65ecd61307565af2
刘彬彬 <956762794@qq.com>
2021-04-29 18:25:52 +0800

    排序

--- LeetCode/二叉树/二叉树的最近公共祖先/lowestCommonAncestor.c
+++ LeetCode/二叉树/二叉树的最近公共祖先/lowestCommonAncestor.c
@@ -8,5 +8,26 @@ struct TreeNode
 };
 struct TreeNode *lowestCommonAncestor(struct TreeNode *root, struct TreeNode *p, struct TreeNode *q)
 {
-  
+  if (root == q || root == p || !root)
+  {
+    return root;
+  }
+  struct TreeNode *left = lowestCommonAncestor(root->left, p, q);
+  struct TreeNode *right = lowestCommonAncestor(root->right, p, q);
+  if (left != NULL && right != NULL)
+  {
+    return root;
+  }
+  if (left == NULL && right != NULL)
+  { /* 左子树未发现p、q、右子树发现p、q */
+    return right;
+  }
+  else if (left != NULL && right == NULL)
+  { /* 右子树未发现p、q、左子树发现p、q */
+    return left;
+  }
+  else
+  { /* 左、右子树均未发现p、q */
+    return NULL;
+  }
 }
