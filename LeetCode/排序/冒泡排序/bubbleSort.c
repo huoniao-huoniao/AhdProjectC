d4e3d8acbf20a50db6518505c17e69950ca8607d
刘彬彬 <956762794@qq.com>
2021-04-30 17:53:53 +0800

    排序

--- LeetCode/排序/冒泡排序/bubbleSort.c
+++ LeetCode/排序/冒泡排序/bubbleSort.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
-void doubbleSort(int arr[],int lenght)
+void doubbleSort(int arr[], int lenght)
 {
-  for (int i = 0; i < lenght-1; i++)
+  for (int i = 0; i < lenght - 1; i++)
   {
-    
+    for (int j = 0; j < lenght - 1 - i; j++)
+    {
+      if (arr[i] > arr[j])
+      {
+        swap(arr, i, j);
+      }
+    }
   }
-  
 }
 
 void swap(int arr[], int i, int j)
