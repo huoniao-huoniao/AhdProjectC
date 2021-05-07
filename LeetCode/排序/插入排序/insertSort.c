ef9daf72ceafbc85f39389e3a508524b6dead13e
刘彬彬 <956762794@qq.com>
2021-05-07 16:03:57 +0800

    插入排序

---
+++ LeetCode/排序/插入排序/insertSort.c
@@ -0,0 +1,29 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+#include <string.h>
+int *insertSort(int *nums, int numsSize, int *returnSize)
+{
+  *returnSize = numsSize;
+  if (numsSize == 0)
+    return NULL;
+  if (numsSize == 1)
+    return nums;
+  for (int i = 1; i < numsSize; i++)
+  {
+    int j = i - 1;
+    while (j >= 0 && nums[i] < nums[j])
+    {
+      swap(nums, i, j);
+      j--;
+    }
+  }
+  return nums;
+}
+
+void swap(int *nums, int i, int j)
+{
+  int temp = nums[i];
+  nums[i] = nums[j];
+  nums[j] = temp;
+}
